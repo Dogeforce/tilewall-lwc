9a6642316ae371de556767632292200d356805c2
Renato Oliveira <renato.oliveira@dogeforce.com>
2023-06-30 09:22:55 -0300

    Return early on the days getter. Adds spaces through the code to improve readability.

--- force-app/main/default/lwc/tilewall/tilewall.js
+++ force-app/main/default/lwc/tilewall/tilewall.js
@@ -22,17 +22,21 @@ export default class Tilewall extends LightningElement {
     selectedDay
 
     get days() {
-        if (this.weeks && this.weeks[0]) {
-            let dayList = []
-            for (let day = 0; day < 7; day++) {
-                dayList.push({
-                    name: day % 2 == 0
-                        ? Intl.DateTimeFormat(LOCALE, { weekday: "long" }).format(this.weeks[0].days[day].date)
-                        : ''
-                })
-            }
-            return dayList
+        if (!this.weeks || !this.weeks[0]) {
+            return [];
         }
+
+        let dayList = []
+
+        for (let day = 0; day < 7; day++) {
+            dayList.push({
+                name: day % 2 == 0
+                    ? Intl.DateTimeFormat(LOCALE, { weekday: "long" }).format(this.weeks[0].days[day].date)
+                    : ''
+            })
+        }
+
+        return dayList;
     }
 
     get gridCSS() {
@@ -53,29 +57,34 @@ export default class Tilewall extends LightningElement {
             let oneYearAgo = new Date(today.getTime() - ONE_YEAR_MILIS)
             let weeks = []
             let offset = 0;
+
             if (oneYearAgo.getDay() != FIRSTDAYOFWEEK) {
                 offset = FIRSTDAYOFWEEK - oneYearAgo.getDay()
             }
-            console.log('offset: ' + offset)
+
             for (let day = 0; day <= 365 - offset; day++) {
                 let weekNumber = Math.floor(day / 7)
+
                 if (weeks[weekNumber] === undefined) {
                     weeks[weekNumber] = {
                         id: 'week_' + (weekNumber + 1),
                         days: []
                     }
                 }
+
                 let thatDay = new Date(oneYearAgo.getTime() + (ONE_DAY_MILIS * (day + offset)))
                 let records = res.filter(el => {
                     return new Date(new Date(el.param).getTime() + (thatDay.getTimezoneOffset() * 60 * 1000)).toDateString() == thatDay.toDateString()
                 })
                 let count = records.length != 0 ? records[0].amount : 0
+
                 weeks[weekNumber].days.push({
                     id: day,
                     date: thatDay,
                     count: count
                 })
             }
+
             this.weeks = weeks
         }).catch(err => {
             console.error(err)
